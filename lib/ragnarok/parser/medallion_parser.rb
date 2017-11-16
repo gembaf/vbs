93b808e5249146fafe0053a05375302c69498299
gembaf <gembaf.github@gmail.com>
2017-11-17 00:42:26 +0900

    メダリオンの抽出処理を追加

--- lib/ragnarok/parser/medallion_parser.rb
+++ lib/ragnarok/parser/medallion_parser.rb
@@ -2,11 +2,36 @@ module Ragnarok
   module Parser
     class MedallionParser
       def initialize(doc)
-        @doc = doc
+        @text = doc.text
       end
 
+      # @text => 【力】 力のメダリオン　レアリティー:1 / 必要生成資源:250 / 称号付与武勲:3  †
       def parse
-        @doc
+        elem = @text.split.map { |t| t.split(/　/) }.flatten
+
+        {
+          short_name: short_name(elem[0]),
+          name: elem[1],
+          reality: reality(elem[2]),
+          resource: resource(elem[4]),
+          cost: cost(elem[6]),
+        }
+      end
+
+      def short_name(text)
+        text[/【(.*?)】/, 1]
+      end
+
+      def reality(text)
+        text.split(/:/).last.to_i
+      end
+
+      def resource(text)
+        text.split(/:/).last.to_i
+      end
+
+      def cost(text)
+        text.split(/:/).last.to_i
       end
     end
   end
--- spec/lib/ragnarok/parser/medallion_parser_spec.rb
+++ spec/lib/ragnarok/parser/medallion_parser_spec.rb
@@ -1,15 +1,63 @@
 describe Ragnarok::Parser::MedallionParser do
-  subject { described_class.new(doc).parse }
-
-  let(:doc) { Nokogiri::HTML.parse(html, nil, 'utf-8') }
+  let(:doc) { Nokogiri::HTML.parse(html, nil, 'utf-8').xpath('//h2').first }
   let(:html) { File.read(Rails.root.join('spec/fixtures/medallion.html')) }
-  let(:result) do
-    {
-    }
+
+  context '#parse' do
+    subject { described_class.new(doc).parse }
+
+    let(:result) do
+      {
+        short_name: '力',
+        name: '力のメダリオン',
+        reality: 1,
+        resource: 250,
+        cost: 3,
+      }
+    end
+
+    it '正常にパースできること' do
+      expect(subject).to eq result
+    end
   end
 
-  it '正常にパースできること' do
-    expect(subject).to eq result
+  context '#short_name' do
+    subject { described_class.new(doc).short_name(text) }
+
+    let(:text) { '【力】' }
+
+    it '正常にパースできること' do
+      expect(subject).to eq '力'
+    end
+  end
+
+  context '#reality' do
+    subject { described_class.new(doc).reality(text) }
+
+    let(:text) { 'レアリティー:1' }
+
+    it '正常にパースできること' do
+      expect(subject).to eq 1
+    end
+  end
+
+  context '#resource' do
+    subject { described_class.new(doc).resource(text) }
+
+    let(:text) { '必要生成資源:250' }
+
+    it '正常にパースできること' do
+      expect(subject).to eq 250
+    end
+  end
+
+  context '#cost' do
+    subject { described_class.new(doc).cost(text) }
+
+    let(:text) { '称号付与武勲:3' }
+
+    it '正常にパースできること' do
+      expect(subject).to eq 3
+    end
   end
 end
 
