4d962fda4930f720013f04cabf9a0d692a36212a
gembaf <gembaf.github@gmail.com>
2017-11-16 23:04:19 +0900

    medallionパース用のサービスを追加

---
+++ app/service/ragnarok.rb
@@ -0,0 +1,2 @@
+module Ragnarok
+end
---
+++ app/service/ragnarok/medallion_parse_service.rb
@@ -0,0 +1,8 @@
+module Ragnarok
+  class MedallionParseService
+    def call
+      original_data = Rails.root.join('lib/assets/ragnarok/medallion.htm')
+    end
+  end
+end
+
