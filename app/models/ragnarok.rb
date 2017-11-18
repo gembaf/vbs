25d2342caeb860ba3ed7d727072c20055e3a3ba7
gembaf <gembaf.github@gmail.com>
2017-11-18 23:34:12 +0900

    リファクタリング

--- app/models/ragnarok.rb
+++ app/models/ragnarok.rb
@@ -1,5 +1,2 @@
 module Ragnarok
-  def self.table_name_prefix
-    'ragnarok_'
-  end
 end
--- app/models/ragnarok/application_record.rb
+++ app/models/ragnarok/application_record.rb
@@ -1,5 +1,7 @@
 module Ragnarok
   class ApplicationRecord < ::ApplicationRecord
+    self.abstract_class = true
+    self.table_name_prefix = 'ragnarok_'
   end
 end
 
