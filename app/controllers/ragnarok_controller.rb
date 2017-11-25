ea27266bda1d24c447e7dde39a018128c8bb3ffe
gembaf <gembaf.github@gmail.com>
2017-11-25 17:43:08 +0900

    リファクタリング

--- app/controllers/ragnarok_controller.rb
+++ app/controllers/ragnarok_controller.rb
@@ -7,28 +7,21 @@ class RagnarokController < ApplicationController
     each_ids = []
 
     @tribe_name = params[:tribe_name]
-    each_ids << finder.tribe(@tribe_name) if @tribe_name.present?
+    each_ids << finder.by_tribe(@tribe_name) if @tribe_name.present?
 
     @protection_name = params[:protection_name]
-    each_ids << finder.protection(@protection_name) if @protection_name.present?
+    each_ids << finder.by_protection(@protection_name) if @protection_name.present?
 
     @skill_name = params[:skill_name]
-    if @skill_name.present?
-      skill = Ragnarok::Skill.find_by(name: @skill_name)
-      skill_ids = Ragnarok::PassiveSkill.where(skill: skill).pluck(:unit_id)
-      skill_ids += Ragnarok::LeaderSkill.where(skill: skill).pluck(:unit_id)
-    end
+    each_ids << finder.by_skill(@skill_name) if @skill_name.present?
 
     unit_ids = Ragnarok::Unit.pluck(:id)
-    each_ids.each do |ids|
-      unit_ids &= ids
-    end
+    each_ids.each { |ids| unit_ids &= ids }
 
     units = Ragnarok::Unit.where(id: unit_ids).limit(30)
-
     @data = [
       units,
-      units.map {|unit| unit.best_title_skills(skill_name: skill.name) },
+      units.map {|unit| unit.best_title_skills(skill_name: @skill_name) },
     ].transpose
   end
 
--- app/finders/ragnarok/unit_finder.rb
+++ app/finders/ragnarok/unit_finder.rb
@@ -1,11 +1,19 @@
 module Ragnarok
   class UnitFinder
-    def tribe(name)
+    def by_tribe(name)
       Ragnarok::Unit.where_like('tribe', name).pluck(:id)
     end
 
-    def protection(name)
+    def by_protection(name)
       Ragnarok::Unit.where_like('protection', name).pluck(:id)
+    end
+
+    def by_skill(name)
+      skill = Ragnarok::Skill.find_by(name: name)
+
+      ids = Ragnarok::PassiveSkill.where(skill: skill).pluck(:unit_id)
+      ids += Ragnarok::LeaderSkill.where(skill: skill).pluck(:unit_id)
+      ids
     end
   end
 end
